b666ccbee56fa9563f47ebf191409ba887502d14
nourasharrof <167745712+nourasharrof@users.noreply.github.com>
2026-04-20 00:15:31 +0300

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -1,21 +1,15 @@
-document.getElementById('bx-menu').addEventListener('click',function (){
-    
-document.querySelector('.nav-links').classList.toggle('active');
-
-
-
+document.getElementById("bx-menu").addEventListener("click", function () {
+  document.querySelector(".nav-links").classList.toggle("active");
 });
-document.forms[0].onsubmit=function(event){
-    let yourName=document.getElementsByClassName('text-Email')[0].value;
-    let yourEmail=document.getElementsByClassName('text-Email')[1].value;
-    if(yourName===""){
-        event.preventDefault();
-        alert('pleas enter your name');
-    }
-    if(yourEmail===""){
-        event.preventDefault();
-        alert('pleas enter your Email');
-    }   
-}
-
-
+document.forms[0].onsubmit = function (event) {
+  let yourName = document.getElementsByClassName("text-Email")[0].value;
+  let yourEmail = document.getElementsByClassName("text-Email")[1].value;
+  if (yourName === "") {
+    event.preventDefault();
+    alert("pleas enter your name");
+  }
+  if (yourEmail === "") {
+    event.preventDefault();
+    alert("pleas enter your Email");
+  }
+};
